cfbdf5c1549e1b4c9163bb68612570c6770cc826
Estanley Santana <contatostanmilke@gmail.com>
2024-09-02 23:04:06 -0300

    some adictions
    dash enhanced with shader and trail effects

---
+++ game_scenes/ghost_sprite.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var fade_out = get_tree().create_tween()
+	fade_out.tween_property(self, "modulate", Color(1,1,1,0), 0.5)
+	fade_out.set_trans(Tween.TRANS_QUART)
+	fade_out.set_ease(Tween.EASE_IN_OUT)
+	await fade_out.finished
+	get_parent().queue_free()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/player/dash_component.gd
+++ scripts/player/dash_component.gd
@@ -6,17 +6,24 @@ extends Node2D
 @export var dash_distance: float = 100
 @export var dash_speed: float = 800
 @export var dash_modifier: float
+@export var ghost_effect:  PackedScene
+@export var node_texture:  Sprite2D
+
+@onready var ghost_timer: Timer = $ghost_timer
 
 var is_dashing: bool = false
+var ghost_instance
+var ghost
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 func dash():
 	is_dashing = true
 	var target_position = (get_global_mouse_position() - node.global_position).normalized()
 	target_position = node.global_position + (target_position * dash_distance)
 	set_dash_speed()
+	_dash_effect()
 	navigation_component.set_destination(target_position)
 
 func set_dash_speed():
@@ -25,3 +32,20 @@ func set_dash_speed():
 func end_dash():
 	node.SPEED = 650
 	is_dashing = false
+	ghost_timer.stop()
+
+func _dash_effect():
+	ghost_instance = ghost_effect.instantiate()
+	node.get_parent().add_child(ghost_instance)
+	ghost = ghost_instance.get_node("ghost_sprite")
+	ghost.texture = node_texture.texture
+	ghost.hframes = node_texture.hframes
+	ghost.vframes = node_texture.vframes
+	ghost.frame = node_texture.frame
+	ghost_instance.global_position = node.global_position
+	
+	
+	ghost_timer.start()
+	
+func _on_ghost_timer_timeout() -> void:
+	_dash_effect()
